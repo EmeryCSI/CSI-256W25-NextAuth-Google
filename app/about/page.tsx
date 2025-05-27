bc13159a22e7da3058b821783dd8c838676dc9f2
Joshua Emery <102991550+EmeryAtRTC@users.noreply.github.com>
2025-05-27 12:34:54 -0700

    Updated

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -11,7 +11,7 @@ export default async function About() {
   const user = session?.user;
   return (
     <div>
-      <h2>About</h2>
+      <h2 className="text-3xl">This is the About Page</h2>
       <h3>User: {user?.name}</h3>
       <p>Email: {user?.email}</p>
       <img src={user?.image ?? ""} alt="User Avatar" />
